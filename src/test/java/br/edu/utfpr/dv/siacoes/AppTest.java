ac5abb858b7e4c08c6abf855d7741afcea931921
Loran Braga <loranpb@gmail.com>
2021-05-20 20:00:05 -0300

    Tentiva de rodar o projeto

---
+++ src/test/java/br/edu/utfpr/dv/siacoes/AppTest.java
@@ -0,0 +1,25 @@
+package br.edu.utfpr.dv.siacoes;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+
+/**
+ * Unit test for simple App.
+ */
+@Testable
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public void getCarbon()
+    {
+        assertTrue( true );
+    }
+
+   
+}
---
+++ src/test/java/br/edu/utfpr/dv/siacoes/Teste.java
@@ -0,0 +1,15 @@
+package br.edu.utfpr.dv.siacoes;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+@Testable
+public class Teste {
+  @Test
+  public void getCarbon()
+  {
+    assertTrue( true );
+  }
+}
